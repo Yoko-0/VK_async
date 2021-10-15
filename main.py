25aea36be60bc224417740db1a90ccf53a27a0a3
Yoko <zartem01@gmail.com>
2021-10-15 16:30:59 +0200

    example wait for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from async_vk_lib.async_vk import Async_vk
-
+import asyncio
 
 
 token = '927d0c2eb68bc3197ee29a3634b0de4466b1b66ec0cddbc76142138a1e1c8dc83f2b35fb8354e013c3a25'
@@ -10,7 +10,19 @@ bot = Async_vk(token)
 
 @bot.command(name = 'test')
 async def test(ctx):
-    await ctx.bot.send(ctx.message.user_id, 'Дадова ебать')
+    await ctx.bot.send(ctx.message.user_id, 'Дадова ебать, жду от тебя хуй')
+    user_id = ctx.message.user_id
+
+    def check(ctx):
+        return user_id == ctx.message.user_id and ctx.message.text == 'хуй'
+
+    try:
+        ctx = await ctx.bot.wait_for(check, timeout = 10)
+        await ctx.bot.send(ctx.message.user_id, 'получил хуй')
+
+    except asyncio.TimeoutError:
+        await ctx.bot.send(ctx.message.user_id, 'не дождался хуя')
+
 
 @bot.command(name = 'print')
 async def test_print(ctx):
